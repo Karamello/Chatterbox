6754376273c1c314021808b3ecfb92c4539dbf3b
Karamello <matt.riley95@hotmail.co.uk>
2017-09-26 13:00:46 +0100

    Code cleaned and made more pythonic

--- src/client.py
+++ src/client.py
@@ -3,22 +3,31 @@ import sys
 import select
 from internals import message
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 8080))
-print("Connected to server")
+# Server variables
+host = ''
+port = 8080
 
+# Create and connect socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
+
+# Create list on input streams, i.e stdin and the socket
 reading = [sock, sys.stdin]
 
+# Message types enum like
 NORMAL = 0
-while True:
-    read, _, _ = select.select(reading, [], [], 1)
 
-    for s in read:
-        if s == sys.stdin:
+while True:
+    in_stream, _, _ = select.select(reading, [], [], 1)
+
+    for src in in_stream:
+        # If input stream is stdin, send message
+        if src == sys.stdin:
             message.send_msg(NORMAL, sys.stdin.readline(), sock)
+        # Else it's the socket, so read it and display it
         else:
-            msgtype, text = message.receive_msg_from(sock)
-            message.print_message(msgtype, text)
+            msg_type, text = message.receive_msg_from(sock)
+            message.print_message(msg_type, text)
 
 
 
--- src/server.py
+++ src/server.py
@@ -2,36 +2,46 @@ import socket
 import select
 from internals import message
 
+# Server variables
 host = ''
 port = 8080
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((host, port))
-sock.setblocking(0)
-sock.listen(1)
+# Create socket
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Allow socket to reuse address
+server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Bind socket and listen for incoming connections
+server_sock.bind((host, port))
+server_sock.setblocking(0)
+server_sock.listen(1)
 
-sockets = []
+# Maintain list of client sockets only
+client_sockets = []
 
+# Message types
 NORMAL = 0
 
-def broadcast(msgtype, msgtext, sockets):
-    for s in sockets:
-        message.send_msg(msgtype, msgtext + "\n", s)
+
+# Broadcast message to all clients
+def broadcast(msg_type, msg_text, sockets):
+    for sock in sockets:
+        message.send_msg(msg_type, msg_text + "\n", sock)
 
 
 while True:
-    read, _, _ = select.select(sockets + [sock] , [], [])
+    read, _, _ = select.select(client_sockets + [server_sock], [], [])
 
     for s in read:
-        if s == sock:
-            conn, addr = sock.accept()
+        # If the socket to read is the server socket, new connection ready
+        if s == server_sock:
+            conn, addr = server_sock.accept()
             print("Connceted to {}".format(addr))
-            sockets.append(conn)
+            client_sockets.append(conn)
             conn.setblocking(0)
+        # Else, a client has data - broadcast it
         else:
-            msgtype, data = message.receive_msg_from(s)
-            message.print_message(msgtype, data)
-            broadcast(msgtype, data, sockets)
+            msg_type, data = message.receive_msg_from(s)
+            message.print_message(msg_type, data)
+            broadcast(msg_type, data, client_sockets)
 
 

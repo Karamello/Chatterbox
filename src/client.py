6e6f019a19dd39b97b68832d8f33687398149e22
Karamello <matt.riley95@hotmail.co.uk>
2017-09-26 15:58:29 +0100

    Added support for named connections

--- src/client.py
+++ src/client.py
@@ -1,7 +1,9 @@
 import socket
 import sys
 import select
+import time
 from internals import message
+
 
 # Server variables
 host = ''
@@ -11,11 +13,19 @@ port = 8080
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
 
+# Get username and send to server
+username = raw_input("Enter username: ")
+username = username.strip() + "\n"
+message.send_msg(message.USER, username, sock)
+
 # Create list on input streams, i.e stdin and the socket
 reading = [sock, sys.stdin]
 
-# Message types enum like
-NORMAL = 0
+
+def pretty_print_message(message):
+    msg_time = time.strftime("%H:%M:%S")
+    print msg_time, message
+
 
 while True:
     in_stream, _, _ = select.select(reading, [], [], 1)
@@ -23,12 +33,15 @@ while True:
     for src in in_stream:
         # If input stream is stdin, send message
         if src == sys.stdin:
-            message.send_msg(NORMAL, sys.stdin.readline(), sock)
+            message.send_msg(message.NORMAL, sys.stdin.readline(), sock)
         # Else it's the socket, so read it and display it
         else:
-            msg_type, text = message.receive_msg_from(sock)
-            message.print_message(msg_type, text)
-
+            try:
+                msg_type, text = message.receive_msg_from(sock)
+            except RuntimeError:
+                pretty_print_message("Lost connection to the server")
+                sys.exit(0)
+            pretty_print_message(text)
 
 
 
